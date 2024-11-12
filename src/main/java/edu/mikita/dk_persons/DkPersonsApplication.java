cfa090034174eecbb44c9347cd06df21aab2d407
slipk <slipknoshka777@gmail.com>
2024-11-12 22:22:53 +0300

    init

---
+++ src/main/java/edu/mikita/dk_persons/DkPersonsApplication.java
@@ -0,0 +1,13 @@
+package edu.mikita.dk_persons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkPersonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkPersonsApplication.class, args);
+	}
+
+}
