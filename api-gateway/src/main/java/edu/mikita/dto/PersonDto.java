4f912e8f179d81fdbddffc3e193cdbb3b48d9ff5
slipk <slipknoshka777@gmail.com>
2024-12-21 23:32:18 +0300

    add api gateway service

---
+++ api-gateway/src/main/java/edu/mikita/dto/PersonDto.java
@@ -0,0 +1,7 @@
+package edu.mikita.dto;
+
+public record PersonDto(String uid,
+                        String firstName,
+                        String lastName,
+                        String email) {
+}
